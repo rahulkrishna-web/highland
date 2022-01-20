98a8b526e2293b698c169001bcc93b576a46fd03
Rahul Krishna <rahulkrishna.web@gmail.com>
2022-01-20 13:00:10 +0530

    Add material theme

---
+++ components/mainAppbar.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+export default function MainAppbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" >
+            Highland
+          </Typography>
+          <Box sx={{ flexGrow: 1, pl: 5 }}>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Buy Land</Button>
+          <Button color="inherit">Support</Button>
+          </Box>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
