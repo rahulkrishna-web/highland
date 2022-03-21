9d6fd82548e89682cf655e89f799cf33e4dd2cf2
Rahul Krishna <rahulkrishna.web@gmail.com>
2022-03-21 16:51:29 +0530

    update homepage text.

--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,22 @@ export default function Home() {
       </Typography>
       </Paper>
     </Box>
+    <Box sx={{px: 2, my: 2}}>
+      <Paper sx={{p: 2}}>
+      <Typography variant="body1" gutterBottom>
+    Our Current world is a mix bag of real and virtual. The future we see will be primarily virtual.<br/><br/>
+
+In this technically driven age , technology  is changing and moving forward  at an immense pace. We all are witnessing more and more science fictions as virtual media which we think might turn into reality soon.
+<br/><br/>
+In the existing time we are already spending half of our time on social media and on other social platform. We also see more virtual realities coming into existence. 
+<br/><br/>
+There’s a long road ahead of us, as a new virtual future is slowly becoming more as a reality, but it may not be as long as we think.We strongly believe that we are creating the stepping stone by laying the foundation of a virtual real estate meta world.
+<br/><br/>
+We are providing the opportunity to people to be a part of this virtual real estate meta world future as we are launching this Alpha platform where you can be a founding brick owner of this new future. 
+      </Typography>
+      </Paper>
+    
+    </Box>
 
     </div>
   )
